49a085b1ab8cf4f4a49ffe814f70144912184cec
root <root@localhost.localdomain>
2022-07-18 18:40:37 +0800

    update Fastjson 1.2.80

---
+++ Fastjson 1.2.80/src/main/java/com/example/fastjson/JSONController.java
@@ -0,0 +1,30 @@
+package com.example.fastjson;
+
+import com.alibaba.fastjson.parser.ParserConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JSONController {
+    @RequestMapping(value={"/"}, method={org.springframework.web.bind.annotation.RequestMethod.GET}, produces={"application/json;charset=UTF-8"})
+    @ResponseBody
+    public Object getUser()
+    {
+        User user = new User();
+        user.setName("Bob");
+        user.setAge(Integer.valueOf(25));
+
+        return user;
+    }
+
+    @RequestMapping(value={"/"}, method={org.springframework.web.bind.annotation.RequestMethod.POST}, produces={"application/json;charset=UTF-8"})
+    @ResponseBody
+    public Object setUser(@RequestBody User user)
+    {
+        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        user.setAge(Integer.valueOf(20));
+        return user;
+    }
+}
